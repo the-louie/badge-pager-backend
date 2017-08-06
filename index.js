6d52641b83897cccb29ce8eadcefdf47958b00e2
Anders Green <louie@louie.se>
2017-08-06 11:30:57 +0200

    add db support

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.write("hej");
   response.end();
 });
 
 app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM badge_users', function(err, result) {
+  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+    client.query('SELECT * FROM badge_users', function (err, result) {
       done();
       if (err) {
-          console.error(err);
-          response.send("Error " + err);
+        console.error(err);
+        response.send("Error " + err);
       } else {
-          response.render('pages/db', {results: result.rows} );
-        }
+        response.send(JSON.stringify(result.rows, null, 2));
+      }
     });
   });
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
