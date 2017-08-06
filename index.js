e2f43d8be507ff3c44275bc7e83463f231168a94
Anders Green <louie@louie.se>
2017-08-06 15:53:49 +0200

    first working version

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const pg = require('pg');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
+const mqtt = require('mqtt');
 
 /**
  *
@@ -23,7 +24,8 @@ const generateId = (salt) => {
 
 app.set('port', (process.env.PORT || 5000));
 
-const jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json();
+const urlEncoder = bodyParser.urlencoded();
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -31,36 +33,102 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (request, response) {
-  response.render("pages/index");
+  if (mqttClient.connected) {
+    response.render("pages/index");
+  } else {
+    response.send("Please come back later, a minute or two");
+    response.end();
+  }
 });
 
-app.post('/send', jsonParser, (request, response) => {
-  let messageId = generateId(request.body.nick + request.body.message);
+const getUserFromNick = (nick) => new Promise((resolve, reject) => {
   pg.connect(process.env.DATABASE_URL, (err, client, done) => {
-    client.query('INSERT INTO badge_messages (messageid, sender, target, message) VALUES ($1,$2,$3,$4)', [], (err, result) => {
-      if(err) return console.log("ERROR", err);
-      if(result) return console.log("RESULT", result);
-      response.send(messageId);
+    client.query('SELECT * FROM badge_users WHERE nick = $1', [nick], (err, result) => {
+      if (err) { reject(); return; }
+      else if (result.rowCount === 0) { reject(); return; }
+      else { resolve(result.rows[0]); }
     });
   });
 });
 
-app.get('/response/:messageId', (request, response) => {
+// const mqttClient  = mqtt.connect('mqtt://test.mosquitto.org');
+const mqttClient  = mqtt.connect('wxs://test.mosquitto.org');
+
+mqttClient.on('connect', function () {
+  mqttClient.subscribe('sha2017pager/swe/replies');
+  console.log("MQTT connected");
+});
+
+mqttClient.on('message', function (topic, message) {
+  // message is Buffer
+  // {"id": "680946cf38867399a0c38c5e39cc793af574b3f3", "text": "ack"}
+  try {
+    data = JSON.parse(message);
+  } catch (e) {
+    console.log("Invalid JSON in response", message);
+    return;
+  }
+
   pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-    client.query('SELECT * FROM badge_users', function (err, result) {
+    client.query('INSERT INTO badge_responses (messageid, response) VALUES ($1, $2)', [data.id, data.text], function (err, result) {
+      if (err) { console.error(err); }
+      done();
+    });
+  });
+
+  console.log(message.toString());
+});
+
+async function sendHandler(request, response) {
+  let targetNick = request.body.target;
+  let senderNick = request.body.sender;
+  let message = request.body.message;
+  let messageId = generateId(targetNick + message + senderNick);
+
+  let sender = await getUserFromNick(senderNick);
+  let target = await getUserFromNick(targetNick);
+
+  pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+    client.query(
+        'INSERT INTO badge_messages (messageid, sender, target, message) VALUES ($1,$2,$3,$4)',
+        [messageId, sender.id, target.id, message],
+        (err, result) => {
+          if(err) {
+            resonse.send("ERR");
+            return console.log("ERROR", err);
+          }
+          const mqttTarget = `sha2017pager/swe/${target.badgeid}`;
+          console.log(`Sending '${message}' to '${mqttTarget}'.`);
+          const payload = JSON.stringify({sender: sender.nick, text: message, id: messageId});
+          mqttClient.publish(mqttTarget, payload, null, console.log);
+          response.send(messageId);
+        });
+  });
+}
+
+app.post('/send', urlEncoder, sendHandler);
+
+app.get('/response/:messageId', (request, response) => {
+  response.render(
+    "pages/response",
+    {
+      messageId: request.params.messageId
+    }
+  );
+});
+
+app.post('/response/:messageId', (request, response) => {
+  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+    client.query('select message, response from badge_responses r left join badge_messages m on m.messageid=r.messageid where r.messageid=$1', [request.params.messageId], function (err, result) {
       done();
       if (err) {
         console.error(err);
-        response.send("Error " + err);
+        response.end();
       } else {
-        if (result.rows.length() > 0) {
-          response.render(
-            "pages/response",
-            {
-              response: result.rows[0].response,
-              messageId: request.params.messageId
-            }
-          );
+        if (result.rowCount > 0) {
+          response.send(result.rows[0]);
+        } else {
+          response.end();
         }
       }
     });
