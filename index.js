6aa1445fd657b667fc72f633d2a9f35a296bc9d0
Anders Green <louie@louie.se>
2017-08-06 12:50:32 +0200

    test first db stuff

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var app = express();
-var pg = require('pg');
+/*jshint esversion: 6 */
+const express = require('express');
+const app = express();
+const pg = require('pg');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+
+/**
+ *
+ *
+ */
+
+const generateId = (salt) => {
+  let current_date = (new Date()).valueOf().toString();
+  return crypto.createHash('sha1').update(current_date + salt).digest('hex');
+}
+
+ /**
+  *
+
+
+  */
 
 app.set('port', (process.env.PORT || 5000));
 
+const jsonParser = bodyParser.json()
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -14,8 +34,37 @@ app.get('/', function (request, response) {
   response.render("pages/index");
 });
 
-app.post('/send', (request, response) => {
-  response.send("OK");
+app.post('/send', jsonParser, (request, response) => {
+  let messageId = generateId(request.body.nick + request.body.message);
+  pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+    client.query('INSERT INTO badge_messages (messageid, sender, target, message) VALUES ($1,$2,$3,$4)', [], (err, result) => {
+      if(err) return console.log("ERROR", err);
+      if(result) return console.log("RESULT", result);
+      response.send(messageId);
+    });
+  });
+});
+
+app.get('/response/:messageId', (request, response) => {
+  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+    client.query('SELECT * FROM badge_users', function (err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        response.send("Error " + err);
+      } else {
+        if (result.rows.length() > 0) {
+          response.render(
+            "pages/response",
+            {
+              response: result.rows[0].response,
+              messageId: request.params.messageId
+            }
+          );
+        }
+      }
+    });
+  });
 });
 
 app.get('/db', function (request, response) {
